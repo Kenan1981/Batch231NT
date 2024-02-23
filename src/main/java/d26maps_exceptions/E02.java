04fe53e328f4ddb78404b61b738fb66f03f6b8fd
Kenan <kenanuyanik@gmail.com>
2024-02-23 23:35:56 +0300

    First version

--- src/main/java/d26maps_exceptions/E02.java
+++ src/main/java/d26maps_exceptions/E02.java
@@ -1,2 +1,22 @@
-package d26maps_exceptions;public class E02 {
+package d26maps_exceptions;
+
+public class E02 {
+    public static void main(String[] args) {
+
+        //2) NumberFormatException : Bir metnin sayisal bir deger olarak donusturulememesi durumunda olusur
+
+        String s="1234a";
+        convertStringToInt(s);
+
+
+
+
+    }
+    public static void convertStringToInt(String s){
+        int intS=Integer.valueOf(s);
+        System.out.println(intS+1);//1'i dönüştürüp dönüştürmediğini anlamak için yazdık.
+    }
+
+
+
 }
--- src/main/java/d26maps_exceptions/Maps02.java
+++ src/main/java/d26maps_exceptions/Maps02.java
@@ -1,2 +1,27 @@
-package d26maps_exceptions;public class Maps02 {
+package d26maps_exceptions;
+
+import java.util.HashMap;
+
+public class Maps02 {
+    public static void main(String[] args) {
+
+        //HasMap arka planda nasıl çalışıryor?
+
+        HashMap<String, String> capitals = new HashMap<>();
+        capitals.put("USA","Washington");
+        capitals.get("USA");
+
+        capitals.put("Italy","Rome");
+        capitals.put("Turkiye","Ankara");
+
+
+        capitals.get("Turkiye");
+        capitals.get("Turkiye");
+
+
+        capitals.put("Turkiye","Istanbul");//Ankarayi siler
+
+
+
+    }
 }
